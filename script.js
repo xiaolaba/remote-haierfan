55e317fa8688b39e8c972776b6a55726a306f725
xiaolaba <xiao_laba_cn@yahoo.com>
2024-02-24 16:28:24 +0800

    Add files via upload
    Signed-off-by: xiaolaba <xiao_laba_cn@yahoo.com>

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const audioButtons = document.querySelectorAll('.audio-button');
-  const audioPlayer = document.getElementById('audioPlayer');
+document.getElementById('audio-button-1').onclick = function() {
+    playSound('1.wav');
+};
 
-  audioButtons.forEach(button => {
-    button.addEventListener('click', function() {
-      const soundFile = this.dataset.sound;
-      audioPlayer.src = soundFile;
-      audioPlayer.play();
-    });
-  });
-});
+document.getElementById('audio-button-2').onclick = function() {
+    playSound('2.wav');
+};
+
+document.getElementById('audio-button-3').onclick = function() {
+    playSound('3.wav');
+};
+
+document.getElementById('audio-button-4').onclick = function() {
+    playSound('4.wav');
+};
+
+document.getElementById('audio-button-5').onclick = function() {
+    playSound('5.wav');
+};
+
+document.getElementById('audio-button-6').onclick = function() {
+    playSound('6.wav');
+};
+
+function playSound(soundFile) {
+    const audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.src = soundFile;
+    audioPlayer.play();
+}
