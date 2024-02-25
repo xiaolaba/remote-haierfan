dca680d11348b5110b32afbc4e10b66c779f47f9
xiaolaba <xiao_laba_cn@yahoo.com>
2024-02-25 12:04:49 +0800

    Add files via upload
    Signed-off-by: xiaolaba <xiao_laba_cn@yahoo.com>

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 document.getElementById('audio-button-1').onclick = function() {
-    playSound('1.wav');
+    playSound('1-modulated_親嘴不要臉.wav');
 };
 
 document.getElementById('audio-button-2').onclick = function() {
-    playSound('2.wav');
+    playSound('2-modulated_親嘴不要臉.wav');
 };
 
 document.getElementById('audio-button-3').onclick = function() {
-    playSound('3.wav');
+    playSound('3-modulated_親嘴不要臉.wav');
 };
 
 document.getElementById('audio-button-4').onclick = function() {
-    playSound('4.wav');
+    playSound('4-modulated_親嘴不要臉.wav');
 };
 
 document.getElementById('audio-button-5').onclick = function() {
-    playSound('5.wav');
+    playSound('5-modulated_親嘴不要臉.wav');
 };
 
 document.getElementById('audio-button-6').onclick = function() {
-    playSound('6.wav');
+    playSound('6-modulated_親嘴不要臉.wav');
 };
 
 function playSound(soundFile) {
